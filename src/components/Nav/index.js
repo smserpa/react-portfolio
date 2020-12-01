157dee4a5a6f0cae5fbb229763e789f4f0f2faee
Stefanie Serpa <smserpa@gmail.com>
2020-12-01 12:19:50 -0800

    added nav-brand to route to home in navbar

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,5 +1,8 @@
 import { Component } from "react";
-import { BrowserRouter as Router, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+import AboutMe from "../AboutMe";
+import HomePage from "../HomePage";
+import Portfolio from "../Portfolio";
 
 class Nav extends Component {
   render() {
@@ -7,6 +10,11 @@ class Nav extends Component {
       <Router>
         <div className="container p-0" fluid={true}>
           <div className="navbar border-bottom" bg="transparent" expand="lg">
+          <div className="navbar-header">
+                <Link className="navbar-brand" to="/">
+                  Stefanie Serpa
+                </Link>
+              </div>
             <div className="navbar" aria-controls="navbar-toggle" />
             <div className="navbar" id="navbar-toggle">
                 <Link className="nav-link" to="/about">
@@ -20,6 +28,11 @@ class Nav extends Component {
                 </Link>
             </div>
           </div>
+        <Switch>
+        <Route path="/" exact render={() => <HomePage />} />
+        <Route path="/about" exact render={() => <AboutMe />} />
+        <Route path="/portfolio" exact render={() => <Portfolio/>} />
+        </Switch>
         </div>
       </Router>
     );
