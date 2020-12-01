4c57771b8938049f1edd91f29400bd2f292f8721
Stefanie Serpa <smserpa@gmail.com>
2020-12-01 12:16:46 -0800

    structued homepage

---
+++ src/components/HomePage/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function HomePage() {
+  return (
+    <div className="jumbotron bg-transparent jumbotron-fluid p-0">
+      <div class="container">
+        <div className="row justify-content-center">
+          <div className="col-md-8 col-xs-12">
+              
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default HomePage;
