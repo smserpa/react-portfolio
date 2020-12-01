797d3c0460fd12e87906685304813d9af444a9b3
Stefanie Serpa <smserpa@gmail.com>
2020-11-30 18:45:45 -0800

    edited App.js and index.js to render react app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import AboutMe from "./components/AboutMe"
+import Portfolio from "./components/Portfolio";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  render() {
+    return (
+      <div id="colorlib-page">
+        <div id="container-wrap">
+          <div id="colorlib-main">
+            <AboutMe />
+            <Portfolio />
+          </div>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default App;
