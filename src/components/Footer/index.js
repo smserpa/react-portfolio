e0cf710b42cc70e413b23ecd93a24ab00abcff6a
Stefanie Serpa <smserpa@gmail.com>
2020-12-01 12:18:40 -0800

    updated footer component

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -7,7 +7,7 @@ function Footer() {
             <div className="container">
                 <div id="copyright" className="row justify-content-center p-3">
                     <div className="col-md-3 col-xs-12 p-0">
-                        <p>&copy; {new Date().getFullYear()} Copyright: Stefanie Serpa</p>
+                        <p>&copy; {new Date().getFullYear()} Copyright Stefanie Serpa</p>
                     </div>
                 </div>
             </div>
