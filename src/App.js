485a01ee1b6977a53b60bd7eac2c90f99d3cda4c
Stefanie Serpa <smserpa@gmail.com>
2020-12-01 17:05:13 -0800

    removed app.js constructor

--- src/App.js
+++ src/App.js
@@ -1,34 +1,23 @@
 import React, { Component } from "react";
 import Nav from "./components/Nav";
 import HomePage from "./components/HomePage";
-import AboutMe from "./components/AboutMe"
+import AboutMe from "./components/AboutMe";
 import Portfolio from "./components/Portfolio";
 import Footer from "./components/Footer";
-
+import "./App.css";
 
 class App extends Component {
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      headerLinks: [
-        {title: "Home", path: "/"},
-        {title: "About", path: "/about"},
-        {title: "Contact", path: "/contact"},
-        {title: "Portfolio", path: "/portfolio"}
-      ],
-    }
-  }
   render() {
     return (
-        <div className="container-wrap p-0">
-          <Nav />
-          <HomePage />
-          <AboutMe />
-          <Portfolio />
-          <Footer />
-        </div>
-    )
+      <div className="container p-0">
+        <Nav />
+        <HomePage />
+        <AboutMe />
+        <Portfolio />
+        <Footer />
+      </div>
+      
+    );
   }
 }
 
