4431969739907f95e9e0753ff34a381cf355f2a0
Stefanie Serpa <smserpa@gmail.com>
2020-12-02 14:14:15 -0800

    updated home page jumbotron

--- src/components/HomePage/index.js
+++ src/components/HomePage/index.js
@@ -1,11 +1,19 @@
 import React from "react";
+import Portfolio from "../Portfolio";
 import "./styles.css";
 
 function HomePage() {
   return (
-    <div className="jumbotron jumbotron-fluid">
-      
+    <header className="home-page">
+      <div className="container">
+      <h2 className="heading">Stefanie Serpa</h2>
+      <h5 className="heading-text">Full-Stack Web Developer</h5>
+      <div className="btn btn-primary" id="home-button">
+      <a href="portfolio" component={Portfolio} style={{color: "white"}}>Check Out My Work</a>
+        </div> 
     </div>
+    </header>
+    
   );
 }
 
