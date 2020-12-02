e9a0f63a41c0a36bd5e284658e647df355c055b9
Stefanie Serpa <smserpa@gmail.com>
2020-12-01 17:33:11 -0800

    added jumbotron to homepage

--- src/components/HomePage/index.js
+++ src/components/HomePage/index.js
@@ -1,15 +1,10 @@
 import React from "react";
+import "./styles.css";
 
 function HomePage() {
   return (
-    <div className="jumbotron bg-transparent jumbotron-fluid p-0">
-      <div class="container">
-        <div className="row justify-content-center">
-          <div className="col-md-8 col-xs-12">
-              
-          </div>
-        </div>
-      </div>
+    <div className="jumbotron jumbotron-fluid">
+      
     </div>
   );
 }
