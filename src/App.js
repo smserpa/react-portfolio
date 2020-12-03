9d849b6d4bdca86c556ca9c1680469a02f15dedf
Stefanie Serpa <smserpa@gmail.com>
2020-12-03 13:30:11 -0800

    added Contact component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import Nav from "./components/Nav";
 import HomePage from "./components/HomePage";
 import AboutMe from "./components/AboutMe";
+import Contact from "./components/Contact";
 import Portfolio from "./components/Portfolio";
 import Footer from "./components/Footer";
 import "./App.css";
@@ -14,10 +15,14 @@ function App() {
         <Route exact path="/">
           <HomePage />
           <AboutMe />
+          <Contact />
           <Portfolio />
         </Route>
         <Route exact path="/about">
           <AboutMe />
+        </Route>
+        <Route exact path="/contact">
+          <Contact />
         </Route>
         <Route exact path="/portfolio">
           <Portfolio />
