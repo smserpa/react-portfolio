dfe28708a0ff76a5e8fda860965b39063dd739b7
Stefanie Serpa <smserpa@gmail.com>
2020-12-03 13:40:07 -0800

    updated Contact component

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,71 +1,31 @@
-import React, { Component } from "react";
-import axios from "axios";
+import React from "react";
 import "./styles.css";
 
-class Contact extends Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-        name: "",
-        email: "",
-        message: ""
-      }
-    }
+function Contact() {
+  return (
+    <div className="container p-5 mt-5">
+      <h2 id="contact-header">Contact</h2>
+      <div id="contact-body" className="row justify-content-center p-5 mt-5 mb-5">
+      <div className="col-md-2">
+      <p>Name:</p>
+      <p>Location:</p>
+      <p>Education:</p>
+      <p>Email:</p>
+      <p>GitHub:</p>
+      <p>LinkedIn:</p>
+      </div>
+      <div className="col-md-6 text-left">
+        <p>Stefanie Serpa</p>
+        <p>San Diego, CA</p>
+        <p>Full Stack Web Development Bootcamp, UCSD Extension</p>
+        <address><a className="contact-links" href="mailto:smserpa@gmail.com">smserpa@gmail.com</a></address>
+        <p><a className="contact-links" href="https://github.com/smserpa">GitHub</a></p>
+        <p><a className="contact-links" href="https://www.linkedin.com/in/stefanie-serpa-57993610b/">LinkedIn</a></p>
+      </div>
+      </div>
+    </div>
+  )
 
-    handleSubmit(e) {
-        e.preventDefault();
-      
-        axios({
-          method: "POST", 
-          url:"http://localhost:3000/send", 
-          data:  this.state
-        }).then((response)=>{
-          if (response.data.status === 'success') {
-            alert("Message Sent."); 
-            this.resetForm()
-          } else if(response.data.status === 'fail') {
-            alert("Message failed to send.")
-          }
-        })
-      }
-
-    resetForm(){
-        this.setState({name: "", email: "", message: ""})
-      }
-  
-    render() {
-      return(
-        <div className="contact">
-          <form id="contact-form" onSubmit={this.handleSubmit.bind(this)} method="POST">
-            <div className="form-group">
-              <label htmlFor="name">Name</label>
-              <input type="text" className="form-control" value={this.state.name} onChange={this.onNameChange.bind(this)} />
-            </div>
-            <div className="form-group">
-              <label htmlFor="exampleInputEmail1">Email address</label>
-              <input type="email" className="form-control" aria-describedby="emailHelp" value={this.state.email} onChange={this.onEmailChange.bind(this)} />
-            </div>
-            <div className="form-group">
-              <label htmlFor="message">Message</label>
-              <textarea className="form-control" rows="5" value={this.state.message} onChange={this.onMessageChange.bind(this)} />
-            </div>
-            <button type="submit" className="btn btn-primary">Submit</button>
-          </form>
-        </div>
-      );
-    }
-  
-    onNameChange(event) {
-      this.setState({name: event.target.value})
-    }
-  
-    onEmailChange(event) {
-      this.setState({email: event.target.value})
-    }
-  
-    onMessageChange(event) {
-      this.setState({message: event.target.value})
-    }
-  }
+}
 
 export default Contact;
