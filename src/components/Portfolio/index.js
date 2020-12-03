da31eae03dae752a32a85993dfd289d1810e0e70
Stefanie Serpa <smserpa@gmail.com>
2020-12-03 12:33:45 -0800

    added accordian card functionality

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,42 +1,75 @@
 import React from "react";
 import projects from "../../projects.json";
+import "./styles.css";
 
 function Portfolio() {
   return (
     <div id="porfolio" className="container">
-      <div className="row p-2">
+      <h2 className="portfolio-header">Portfolio</h2>
+      <div className="row align-items-center p-4">
         {projects.map((project) => {
+          
+          let collapseId = `#collapse${project.id}-collapsed`;
+          let controlId = `collapse${project.id}-collapsed`;
+          let headingId = `heading${project.id}-collapsed`;
+
           return (
-            <div className="col-md-4 col-xs-8">
+            <div className="col-md-4 col-xs-12">
               <div
-                className="card"
-                id={project.id}
+                className="card mt-3"
+                id="image-card"
                 tableIndex={-1}
                 key={project.id}
               >
-                <div className="card-header" id="heading">
-                  <h5 className="mb-0">
-                    <button
-                      className="btn btn-link"
-                      data-toggle="collapse"
-                      data-target="#collapseOne"
-                      aria-expanded="true"
-                      aria-controls="collapseOne"
-                    ></button>
-                  </h5>
-                  <img
-                    className="img-fluid d-block mx-auto"
-                    src={project.image}
-                    alt={project.title}
-                  />
-                </div>
+                <img
+                  className="card-img-top"
+                  src={project.image}
+                  alt={project.title}
+                />
+              </div>
+              <div
+                className="card mt-3"
+                id="body-card"
+                tableIndex={-1}
+                key={project.id}
+              >
+                <h5 className="card-header">
+                  <a
+                    className="collapsed d-block"
+                    data-toggle="collapse"
+                    href={collapseId}
+                    aria-expanded="true"
+                    aria-controls={controlId}
+                    id={headingId}
+                  >{project.title}
+                    <i className="fa fa-chevron-down pull-right"></i>
+                  </a>
+                </h5>
                 <div
-                  id="collapseOne"
-                  className="collapse show"
-                  aria-labelledby="headingOne"
-                  data-parent="#accordion"
+                  id={controlId}
+                  className="collapse"
+                  aria-labelledby={headingId}
                 >
-                  <div className="card-body"></div>
+                  <div className="card-body">
+                    <p className="card-text"><h4 className="project-name">Description:</h4>{project.description}</p>
+                    <p className="card-text"><h4 className="project-name">Technologies Used:</h4> {project.technology} </p>
+                    <a
+                      href={project.deployed}
+                      className="btn btn-primary m-2"
+                      target="blank"
+                    >
+                      {" "}
+                      Deployed Application{" "}
+                    </a>
+                    <a
+                      href={project.repo}
+                      className="btn btn-primary m-2"
+                      target="blank"
+                    >
+                      {" "}
+                      GitHub Repository{" "}
+                    </a>
+                  </div>
                 </div>
               </div>
             </div>
