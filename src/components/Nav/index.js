0470f863fcbcfe90d56edd0afc05de8ec8fbbfb2
Stefanie Serpa <smserpa@gmail.com>
2020-12-04 10:48:27 -0800

    updated resume link in Nav component to render PDF

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import Resume from "./resume.pdf";
 import "./styles.css";
 
 function Nav() {
@@ -29,7 +30,7 @@ function Nav() {
           </NavLink>
             </li>
             <li className="nav-item">
-              <a className="nav-link" to="/resume.pdf" href={process.env.PUBLIC_URL + "/resume.pdf"} target="blank" style={{color: "white"}} rel="noreferrer">Resume</a>
+              <a className="nav-link" to="/resume" href={Resume} target="_blank" style={{color: "white"}} rel="noreferrer">Resume</a>
             </li>
           </ul>
         </div>
