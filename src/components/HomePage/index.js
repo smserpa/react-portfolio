6665475b75d8b3499ce8fc51ab1629103c16aa2c
Stefanie Serpa <smserpa@gmail.com>
2020-12-04 16:21:58 -0800

    changed styling of text

--- src/components/HomePage/index.js
+++ src/components/HomePage/index.js
@@ -1,20 +1,20 @@
-import React from "react";
-import Portfolio from "../Portfolio";
+import { Component } from "react";
 import "./styles.css";
 
-function HomePage() {
-  return (
-    <header className="home-page">
-      <div className="container">
-      <h2 className="heading">Stefanie Serpa</h2>
-      <h5 className="heading-text">Full-Stack Web Developer</h5>
-      <div className="btn btn-primary" id="home-button">
-      <a href="portfolio" component={Portfolio} style={{color: "black"}}>Check Out My Work</a>
-        </div> 
-    </div>
-    </header>
-    
-  );
+class HomePage extends Component {
+
+  
+  render() {
+    return (
+      <header className="home-page">
+        <div className="container">
+        <h2 className="heading">Stefanie Serpa</h2>
+        <h5 className="heading-text">Full-Stack Web Developer</h5>
+      </div>
+      </header>
+      
+    );
+  }
 }
 
 export default HomePage;
--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -7,7 +7,7 @@ function Nav() {
   return (
     <div className="container p-0" fluid={true}>
       <nav className="navbar navbar-dark fixed-top d-flex" expand="lg">
-        <NavLink className="navbar-brand" to="/">
+        <NavLink className="navbar-brand" to="/" style={{ textShadow: "1px 1px black"}} >
           {" "}
           Stefanie Serpa
         </NavLink>
@@ -15,22 +15,22 @@ function Nav() {
         <div className="navbar navbar-right" aria-controls="navbar-toggle" id="navbar-toggle">
           <ul className="navbar nav d-flex">
             <li className="nav-item active">
-            <NavLink className="nav-link" to="/about" style={{color: "white"}}>
+            <NavLink className="nav-link" to="/about" style={{color: "white", textShadow: "1px 1px black"}}>
             About
           </NavLink>
             </li>
             <li className="nav-item active">
-            <NavLink className="nav-link" to="/contact" style={{color: "white"}}>
+            <NavLink className="nav-link" to="/contact" style={{color: "white", textShadow: "1px 1px black"}}>
             Contact
           </NavLink>
             </li>
             <li className="nav-item active">
-            <NavLink className="nav-link" to="/portfolio" style={{color: "white"}}>
+            <NavLink className="nav-link" to="/portfolio" style={{color: "white", textShadow: "1px 1px black"}}>
             Portfolio
           </NavLink>
             </li>
             <li className="nav-item">
-              <a className="nav-link" to="/resume" href={Resume} target="_blank" style={{color: "white"}} rel="noreferrer">Resume</a>
+              <a className="nav-link" to="/resume" href={Resume} target="_blank" style={{color: "white", textShadow: "1px 1px black"}} rel="noreferrer">Resume</a>
             </li>
           </ul>
         </div>
